39ab06486ef246a988c6356b19b4f3a3c8a62b1d
nguyễn văn phi tước <nguyenvanphituoc@gmail.com>
2017-11-15 11:06:32 +0700

    example angular 4

--- angular/src/app/app.routing.module.ts
+++ angular/src/app/app.routing.module.ts
@@ -1,10 +1,10 @@
 import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
 
-//components
+// components
 import { HeroesComponent } from './components/heroes/heroes.component';
-import { DashboardComponent } from './components/dashboard/dashboard.component'
-import { HeroDetailComponent } from './components/hero-detail/hero-detail.component'
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { HeroDetailComponent } from './components/hero-detail/hero-detail.component';
 
 const appRoutes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
@@ -12,7 +12,7 @@ const appRoutes: Routes = [
   { path: 'detail/:id', component: HeroDetailComponent },
   { path: 'heroes',     component: HeroesComponent }
   ];
-  
+
   @NgModule({
     imports: [
       RouterModule.forRoot(
